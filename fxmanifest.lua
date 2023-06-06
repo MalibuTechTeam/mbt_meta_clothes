50e83105385683f6e2c4f00a9a345cd96be8b776
Moldrok <alessio.aquilano@outlook.it>
2023-06-07 01:40:17 +0200

    refactor(manifest): manifest reorganization

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,30 +1,27 @@
-fx_version "cerulean"
-
+fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54        'yes'
 game         'gta5'
 
 name 'mbt_meta_clothes'
-author "Malibù Tech Team"
-version "1.2.0"
+author 'Malibù Tech Team'
+version '1.2.0'
 repository 'https://github.com/MalibuTechTeam/mbt_meta_clothes'
-description "mbt_meta_clothes"
+description 'mbt_meta_clothes'
 
 dependencies {
-	'/server:5848',
-	'/onesync'
+	'/server:6116',
+	'/onesync',
+    'oxmysql',
 }
 
-ui_page "web/index.html"
+shared_script 'config.lua'
 
-files {
-    "web/index.html",
-    "web/style.css",
-    "web/index.js",
-}
-
-shared_scripts {
-    "config.lua",
+server_scripts{
+    "@oxmysql/lib/MySQL.lua",
+    "modules/functions/**/server.lua",
+    "modules/bridge/**/server.lua",
+    "modules/core/**/server.lua",
 }
 
 client_scripts{
@@ -33,9 +30,10 @@ client_scripts{
     "modules/core/**/client.lua",
 }
 
-server_scripts{
-    "@oxmysql/lib/MySQL.lua",
-    "modules/functions/**/server.lua",
-    "modules/bridge/**/server.lua",
-    "modules/core/**/server.lua",
+ui_page "web/index.html"
+
+files {
+    "web/index.html",
+    "web/style.css",
+    "web/index.js",
 }
